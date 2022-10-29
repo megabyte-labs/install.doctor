a21a8bc1619a037cb4918a8a8aea82b7696898ef
Brian Zalewski <brian@megabyte.space>
2022-10-29 18:04:11 +0000

    Update dotfiles/.local/motd.sh

--- dotfiles/.local/motd.sh
+++ dotfiles/.local/motd.sh
@@ -258,7 +258,10 @@ print_banner() {
       printf "       \\033[%sm%s   OS       \\033[0m         %s\\n\\n" "$UPDATES_ZERO_COLOR" "" "macOS $(sw_vers -productVersion) / Build $(sw_vers -buildVersion)"
       printf "       \\033[%sm%s   UUID     \\033[0m         %s\\n\\n" "$PODMAN_RUNNING_COLOR" "" "$(ioreg -d2 -c IOPlatformExpertDevice | awk -F\" '/IOPlatformUUID/{print $(NF-1)}')"
       printf "       \\033[%sm%s   LAN IP   \\033[0m         %s\\n\\n" "$BANNER_KERNEL_COLOR" "ﯱ" "$(ifconfig en0 2>/dev/null | grep 'inet ' | cut -d ' ' -f 2)"
-      printf "       \\033[%sm%s   Public IP\\033[0m         %s\\n\\n" "$UPDATES_SECURITY_COLOR" "" "$(curl -sSL ifconfig.me)"
+      IP_ADDR_PUB="$(timeout 1 sh -c 'curl -sSL ifconfig.me')"
+      if [ -n "$IP_ADDR_PUB" ]; then
+        printf "       \\033[%sm%s   Public IP\\033[0m         %s\\n\\n" "$UPDATES_SECURITY_COLOR" "" "$(echo "$IP_ADDR_PUB")"
+      fi
     fi
   fi
 }
