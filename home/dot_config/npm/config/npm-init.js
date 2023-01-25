9bc59a0e0f12b34ef1ac785b9edfd9729e291a3b
Brian Zalewski <brian@megabyte.space>
2023-01-25 06:45:30 -0500

    ☁️ chore(automation): Automated update

--- home/dot_config/npm/config/npm-init.js
+++ home/dot_config/npm/config/npm-init.js
@@ -1,7 +1,7 @@
 module.exports = {
-    version: '0.0.1',
-    main: 'index.js',
-    scripts: {
-        start: 'node index.js'
-    }
+  main: 'index.js',
+  scripts: {
+    start: 'node index.js'
+  },
+  version: '0.0.1'
 }
