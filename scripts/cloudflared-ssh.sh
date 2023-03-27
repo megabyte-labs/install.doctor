6dafb231d9497b12b9caaa346ce9cad1907db0d9
Brian Zalewski <brian@megabyte.space>
2023-03-27 19:23:46 -0400

    Latest

--- scripts/cloudflared-ssh.sh
+++ scripts/cloudflared-ssh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# @file local/cloudflared.sh
+# @file scripts/cloudflared-ssh.sh
 # @brief Installs and configures cloudflared for short-lived SSH certificates authenticated via SSO
 # @description
 #     This script ensures Homebrew is installed and then uses Homebrew to ensure `cloudflared` is installed.
@@ -38,10 +38,71 @@
 #
 #     [SSH with short-lived certificates](https://developers.cloudflare.com/cloudflare-one/tutorials/ssh-cert-bastion/)
 
-# @description Ensures Homebrew is installed and loaded into the `PATH` variable if it is not already available
+# @description Ensure dependencies like `git` and `curl` are installed (among a few other lightweight system packages)
+if ! command -v curl > /dev/null || ! command -v git > /dev/null || ! command -v expect > /dev/null || ! command -v rsync > /dev/null; then
+    if command -v apt-get > /dev/null; then
+        # @description Ensure `build-essential`, `curl`, `expect`, `git`, and `rsync` are installed on Debian / Ubuntu
+        sudo apt-get update
+        sudo apt-get install -y build-essential curl expect git rsync
+    elif command -v dnf > /dev/null; then
+        # @description Ensure `curl`, `expect`, `git`, and `rsync` are installed on Fedora
+        sudo dnf install -y curl expect git rsync
+    elif command -v yum > /dev/null; then
+        # @description Ensure `curl`, `expect`, `git`, and `rsync` are installed on CentOS
+        sudo yum install -y curl expect git rsync
+    elif command -v pacman > /dev/null; then
+        # @description Ensure `curl`, `expect`, `git`, and `rsync` are installed on Archlinux
+        sudo pacman update
+        sudo pacman -Sy curl expect git rsync
+    elif command -v zypper > /dev/null; then
+        # @description Ensure `curl`, `expect`, `git`, and `rsync` are installed on OpenSUSE
+        sudo zypper install -y curl expect git rsync
+    elif command -v apk > /dev/null; then
+        # @description Ensure `curl`, `expect`, `git`, and `rsync` are installed on Alpine
+        apk add curl expect git rsync
+    elif [ -d /Applications ] && [ -d /Library ]; then
+        # @description Ensure CLI developer tools are available on macOS (via `xcode-select`)
+        sudo xcode-select -p >/dev/null 2>&1 || xcode-select --install
+    elif [[ "$OSTYPE" == 'cygwin' ]] || [[ "$OSTYPE" == 'msys' ]] || [[ "$OSTYPE" == 'win32' ]]; then
+        # @description Ensure `curl`, `expect`, `git`, and `rsync` are installed on Windows
+        choco install -y curl expect git rsync
+    fi
+fi
+
+# @description Ensure Homebrew is installed and available in the `PATH`
 if ! command -v brew > /dev/null; then
-    bash <(curl -sSL https://install.doctor/brew)
-    
+    if [ -d /home/linuxbrew/.linuxbrew/bin ]; then
+        eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+        if ! command -v brew > /dev/null; then
+            echo "The /home/linuxbrew/.linuxbrew directory exists but something is not right. Try removing it and running the script again." && exit 1
+        fi
+    else
+        # @description Installs Homebrew and addresses a couple potential issues
+        if command -v sudo > /dev/null && sudo -n true; then
+            echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        else
+            echo "Homebrew is not installed. The script will attempt to install Homebrew and you might be prompted for your password."
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || BREW_EXIT_CODE="$?"
+            if [ -n "$BREW_EXIT_CODE" ]; then
+                if command -v brew > /dev/null; then
+                    echo "Homebrew was installed but part of the installation failed. Trying a few things to fix the installation.."
+                    BREW_DIRS="share/man share/doc share/zsh/site-functions etc/bash_completion.d"
+                    for BREW_DIR in $BREW_DIRS; do
+                        if [ -d "$(brew --prefix)/$BREW_DIR" ]; then
+                            sudo chown -R "$(whoami)" "$(brew --prefix)/$BREW_DIR"
+                        fi
+                    done
+                    brew update --force --quiet
+                fi
+            fi
+        fi
+
+        # @description Ensures the `brew` binary is available on Linux machines. macOS installs `brew` into the default `PATH`
+        # so nothing needs to be done for macOS.
+        if [ -d /home/linuxbrew/.linuxbrew/bin ]; then
+            eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+        fi
+    fi
 fi
 
 # @description Ensures `cloudflared` is installed via Homebrew
