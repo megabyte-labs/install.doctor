c67bd247654db4d3ebd57b8f39fcb04f75801b45
Brian Zalewski <59970525+ProfessorManhattan@users.noreply.github.com>
2023-11-04 01:38:54 -0400

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -315,6 +315,8 @@ if ! command -v brew > /dev/null; then
         # so nothing needs to be done for macOS.
         if [ -d /home/linuxbrew/.linuxbrew/bin ]; then
             eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+        elif [ -f /opt/homebrew/bin/brew ]; then
+            eval "$(/opt/homebrew/bin/brew shellenv)"
         fi
     fi
 fi
