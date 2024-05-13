86042c0fe920a1caf51c6ce538eb40b1963d8844
Brian Zalewski <59970525+ProfessorManhattan@users.noreply.github.com>
2024-05-13 04:51:08 +0000

    Latest

--- home/dot_local/bin/post-installx/executable_post-nginx.sh
+++ home/dot_local/bin/post-installx/executable_post-nginx.sh
@@ -13,12 +13,30 @@
 
 if command -v nginx > /dev/null; then
   if [ -d Applications ] && [ -d /System ]; then
+    ### macOS
     logg info 'Skipping installation of NGINX Amplify because macOS is not supported'
+    NGINX_CONFIG_DIR=/usr/local/etc/nginx
   else
+    ### Linux
+    NGINX_CONFIG_DIR=/etc/nginx
     logg info 'Downloading the NGINX Amplify installer script'
     TMP="$(mktemp)"
     curl -sSL https://github.com/nginxinc/nginx-amplify-agent/raw/master/packages/install.sh > "$TMP"
     logg info 'Running the NGINX Amplify setup script'
     API_KEY="{{ if (stat (joinPath .chezmoi.sourceDir ".chezmoitemplates" "secrets" "NGINX_AMPLIFY_API_KEY")) }}{{- includeTemplate "secrets/NGINX_AMPLIFY_API_KEY" | decrypt | trim -}}{{ else }}{{- env "NGINX_AMPLIFY_API_KEY" -}}{{ end }}" sh "$TMP"
+  fi
+  logg info "Ensuring $NGINX_CONFIG_DIR is present" && sudo mkdir -p "$NGINX_CONFIG_DIR"
+  logg info "Copying configuration files from $HOME/.local/etc/nginx to $NGINX_CONFIG_DIR"
+  sudo rsync -av "$HOME/.local/etc/nginx" "$NGINX_CONFIG_DIR"
+  if [ -d /Applications ] && [ -d /System ]; then
+    ### macOS
+    if [ -d "${HOMEBREW_PREFIX:-/opt/homebrew}/etc/nginx" ] && [ ! -L "${HOMEBREW_PREFIX:-/opt/homebrew}/etc/nginx" ]; then
+      logg info "Removing directory at ${HOMEBREW_PREFIX:-/opt/homebrew}/etc/nginx"
+      sudo rm -rf "{HOMEBREW_PREFIX:-/opt/homebrew}/etc/nginx"
+      logg info "Symlinking /usr/local/etc/nginx to ${HOMEBREW_PREFIX:-/opt/homebrew}/etc/nginx"
+      ln -s /usr/local/etc/nginx "${HOMEBREW_PREFIX:-/opt/homebrew}/etc/nginx"
+    else
+      logg info "Skipping symlinking of /usr/local/etc/nginx to ${HOMEBREW_PREFIX:-/opt/homebrew}/etc/nginx because directory symlink already appears to be there"
+    fi
   fi
 fi
