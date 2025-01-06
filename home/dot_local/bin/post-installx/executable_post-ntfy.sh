ce661dd0b6f25e9d52bbf3610bcf2702f11b60d2
Brian Zalewski <59970525+ProfessorManhattan@users.noreply.github.com>
2025-01-06 09:37:09 +0000

    Moved sounds files, added chezmoi apply system

--- home/dot_local/bin/post-installx/executable_post-ntfy.sh
+++ home/dot_local/bin/post-installx/executable_post-ntfy.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 # @file NTFY Dependencies
-# @brief Ensures branding assets and sound files are in system locations. Also, ensures system dependencies are installed
+# @brief Eensures system dependencies are installed
 
 set -Eeuo pipefail
 trap "gum log -sl error 'Script encountered an error!'" ERR
 
 if command -v ntfy > /dev/null; then
-
-    ### Sound files
-    gum log -sl info 'Ensuring shared sound files are synced to system location'
-    sudo mkdir -p /usr/local/share/sounds
-    ### /usr/share/sounds is ideal spot
-    sudo rsync -rtvp "${XDG_DATA_HOME:-$HOME/.local/share}/sounds/" /usr/local/share/sounds
-    
     ### Debian dependency
     if command -v apt-get > /dev/null; then
         gum log -sl info 'Running sudo apt-get update && sudo apt-get install -y python-dbus'
