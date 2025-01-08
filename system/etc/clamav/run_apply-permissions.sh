5a0bf9e962e00ebc85a7fcb85a5e3a88cb579709
Brian Zalewski <59970525+ProfessorManhattan@users.noreply.github.com>
2025-01-08 06:18:33 +0000

    Bug fixes

--- system/etc/clamav/run_apply-permissions.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo find /etc/clamav -type f -exec chmod 600 {} \;
-sudo find /etc/clamav -type f -exec chown clamav:wheel {} \;
-sudo chmod u+x /etc/clamav/clamav-email
--- system/var/log/clamav/run_apply-permissions.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo find /var/log/clamav -type f -exec chmod 600 {} \;
-sudo find /var/log/clamav -type f -exec chown clamav:wheel {} \;
